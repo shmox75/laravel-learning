b5c6d063daa6630968c889a8165e3aac09727bec
shmox75 <shmox75@gmail.com>
2025-08-22 17:56:10 +0100

    ok

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -2,8 +2,12 @@
 
 namespace Database\Factories;
 
+use App\Models\CarType;
+use App\Models\City;
+use App\Models\FuelType;
 use App\Models\Maker;
 use App\Models\Model;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use PhpParser\Node\Expr\AssignOp\Mod;
 use Illuminate\Support\Str;
@@ -31,7 +35,19 @@ class CarFactory extends Factory
             'year' => fake()->year(),
             'price' => (int) fake()->randomFloat(2, 5, 100) * 1000,
             'vin' => strtoupper(Str::random(17)),
-
+            'mileage' => (int) fake()->randomFloat(2, 5, 5000) * 1000,
+            'car_type_id' => CarType::inRandomOrder()->first()->id,
+            'fuel_type_id' => FuelType::inRandomOrder()->first()->id,
+            'user_id' => User::inRandomOrder()->first()->id,
+            'city_id' => City::inRandomOrder()->first()->id,
+            'address' => fake()->address(),
+            'phone' => function ( array $attributes ) {
+                return User::where('id', $attributes['user_id'])
+                    ->phone;
+            },
+            'description' => fake()->text(2000),
+            'published_at' => fake()->optional(0.9)->dateTimeBetween('- 1 month', '+ 1 day'),
+            
         ];
     }
 }
